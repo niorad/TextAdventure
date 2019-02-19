c4f0bf750a84c0f1dcbdbc5b6ded84db72126b20
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-02-19 15:51:35 -0500

    added basic implementation for room

--- rooms.c
+++ rooms.c
@@ -1,1 +1,44 @@
+
 # include "rooms.h"
+
+Room *room(char* description, Item* items) {
+    Room *new_room = NULL;
+    new_room = (Room *) malloc(sizeof(Room));
+
+    if (new_room == NULL) {
+        printf("malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_room->description = description;
+    new_room->items = items;
+    return new_room;
+}
+
+Room *add_room(Room *room, Room *new_room, enum direction d) {
+    switch(d) {
+        case NORTH:
+            room->north = new_room;
+            break;
+        case EAST:
+            room->east = new_room;
+            break;
+        case SOUTH:
+            room->south = new_room;
+            break;
+        case WEST:
+            room->west = new_room;
+            break;
+        case UP:
+            room->up = new_room;
+            break;
+        case DOWN:
+            room->down = new_room;
+            break;
+        default:
+            printf("Not a valid direction!\n");
+            exit(EXIT_FAILURE);
+
+        return room;
+    }
+}
--- rooms.h
+++ rooms.h
@@ -1,9 +1,13 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "items.h"
+
+enum direction {NORTH, SOUTH, EAST, WEST, UP, DOWN};
 
 // struct declaration
-struct typedef room {
+typedef struct room {
   char *description;
   Item *items;
   struct room *north;
@@ -15,43 +19,6 @@ struct typedef room {
 } Room;
 
 // constructor
-Room *room(char* description, Item* items, Room *north, Room  *south, Room *east, Room *west, Room *up, Room *down) {
-    Room *new_room = NULL;
-    new_room = (struct Room *) malloc(sizeof(struct Room));
-    new_room->description = description);
-    new_room->items = items;
+Room *room(char* description, Item* items);
 
-
-
-
-
-
-  return new_room;
-}
-
-Room *addRoom(Room *room, Room *new_room, direction d) {
-    swtich(d){
-        case NORTH:
-            new_room->north = new_room;
-        case EAST:
-            new_room->east = new_room;
-        case SOUTH:
-            new_room->south = new_room;
-        case WEST:
-            new_room->west = new_room;
-        case UP:
-            new_room->up = new_room;
-        case DOWN:
-            new_room->down = new_room;
-
-    }
-
-
-    if(direction == NORTH){
-
-    } else if (direction == EAST) {
-
-    } else if (direction == SOUTH) {
-
-    }
-}
+Room *addRoom(Room *room, Room *new_room, enum direction d);
