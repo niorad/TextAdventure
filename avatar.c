8ed5de910e1a3323fb69d5c339d5f8bd91574e3e
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-02-19 15:48:18 -0500

    added an avatar c file and header to model players and npcs in the game

---
+++ avatar.c
@@ -0,0 +1,54 @@
+/*
+ * prog: avatar
+ * ------------
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+// getter methods
+Item *get_inventory(Avatar *avatar) {
+    return avatar->backpack;
+}
+
+Room *get_location(Avatar *avatar) {
+    return avatar->location;
+}
+
+// setter methods
+void set_location(Avatar avatar, Room* room){
+    avatar->location = room;
+}
+
+
+// adds an item to the backpack
+void add_to_inventory(Avatar *avatar, Item *item) {
+    item->next = avatar->backpack;
+    avatar->backpack = item;
+}
+
+//changes the room that the avatar currently is in
+void go_to_room(Avatar avatar, enum direction d) {
+    Room *room = get_location(avatar);
+    // determines the desired direction and executes the appropriate function
+    switch (d) {
+        case NORTH:
+            set_location(avatar, room->north);
+        case EAST:
+            set_location(avatar, room ->east);
+        case SOUTH:
+            set_location(avatar, room->south);
+        case WEST:
+            set_location(avatar, room->west);
+        case UP:
+            set_location(avatar, room->up);
+        case DOWN:
+            set_location(avatar, room->down);
+        default:
+            printf("not a valid room");
+            exit(EXIT_FAILURE);
+    }
+}
---
+++ avatar.h
@@ -0,0 +1,33 @@
+
+#include "rooms.h"
+#include "items.h"
+
+typedef struct avatar {
+    Room *location;
+    Item *backpack;
+} Avatar;
+
+// constructor
+Avatar *avatar(Room *location, Item *backpack) {
+    Avatar *new_avatar = NULL;
+    new_avatar = (Avatar *) malloc(sizeof(Avatar));
+
+    if (new_sprite == NULL) {
+        printf("malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_avatar->location = location;
+    new_avatar->backpack = backpack;
+    return new_room;
+}
+
+// getters
+Room *get_location(Avatar *avatar);
+
+Item *get_inventory(Avatar *avatar);
+
+// setters
+void *set_location(Avatar *avatar);
+
+//void *set_inventory(Avatar *avatar);
