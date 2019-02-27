807576f0128cae78f643d9ef3338ba8a88bf18df
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-02-27 15:18:04 -0500

    hardcoded the game

--- adventure.c
+++ adventure.c
@@ -2,6 +2,8 @@
 #include "avatar.h"
 #include <ctype.h>
 
+#define BUFFER_SIZE 500
+
 /*
  * function: get_command
  * ---------------------
@@ -16,16 +18,17 @@
  * returns  :  void
  */
 
-void get_command(Avatar *avatar) {
-	char input[30], command[5] = "", arg[25] = "";
+int get_command(Avatar *avatar) {
+	char input[BUFFER_SIZE], command[BUFFER_SIZE / 5] = "", arg[4 * BUFFER_SIZE / 5] = "";
 	Room *curr_room = get_location(avatar);
 	bool invalid_command = false;
 	// all functions that take arguments return either 0 (if they pass) or -1 (if they fail)
-	int invalid_arg = 0;
+	int arg_num = 0;
 	// gets the command that the user entered
 	do {
 		invalid_command = false;
-		fgets(input, 30, stdin);
+		printf("what do you do: ");
+		fgets(input, BUFFER_SIZE, stdin);
 		// puts the input into lower-case so user can input either.
 		for (int j = 0; input[j]; j++) {
 			input[j] = tolower(input[j]);
@@ -42,47 +45,44 @@ void get_command(Avatar *avatar) {
 
 		// reads for which command the user has entered
 		if(strcmp(command, "look") == 0) {
-			printf("you look around and find yourself in ");
-			printf("%s\n", curr_room->description);
-			printf("after some searching, you discover \n");
-			list_items(&(curr_room->items));
-			printf("you can go to: \n");
-			list_connections(curr_room);
+			printf("\n");
+			look(avatar);
 		} else if(strcmp(command, "go") == 0) {
 			// reads for which arg the user has entered for the command "go"
-			if (strcmp(arg, "north")) {
-				invalid_arg = go_to_room(avatar, NORTH);
-			} else if (strcmp(arg, "south")) {
-				invalid_arg = go_to_room(avatar, SOUTH);
-			} else if (strcmp(arg, "east")) {
-				invalid_arg = go_to_room(avatar, EAST);
-			} else if (strcmp(arg, "west")) {
-				invalid_arg = go_to_room(avatar, WEST);
-			} else if (strcmp(arg, "up")) {
-				invalid_arg = go_to_room(avatar, UP);
-			} else if (strcmp(arg, "down")) {
-				invalid_arg = go_to_room(avatar, DOWN);
+			if (strcmp(arg, "north") == 0) {
+				arg_num = go_to_room(avatar, NORTH);
+				// if (arg_num != -1) look(avatar);
+			} else if (strcmp(arg, "south") == 0) {
+				arg_num = go_to_room(avatar, SOUTH);
+			} else if (strcmp(arg, "east") == 0) {
+				arg_num = go_to_room(avatar, EAST);
+			} else if (strcmp(arg, "west") == 0) {
+				arg_num = go_to_room(avatar, WEST);
+			} else if (strcmp(arg, "up") == 0) {
+				arg_num = go_to_room(avatar, UP);
+			} else if (strcmp(arg, "down") == 0) {
+				arg_num = go_to_room(avatar, DOWN);
 			}
 		} else if(strcmp(command, "take") == 0) {
-			invalid_arg = take(avatar, arg);
+			arg_num = take(avatar, arg);
 		} else if (strcmp(command, "use") == 0) {
-			invalid_arg = use(avatar, arg);
+			arg_num = use(avatar, arg);
 		} else if (strcmp(command, "drop") == 0) {
-			invalid_arg = drop(avatar, arg);
-		} else if(strcmp(command, "examine") == 0) {
-
+			arg_num = drop(avatar, arg);
 		} else if (strcmp(command, "inventory") == 0) {
-			printf("your inventory: \n")
+			printf("\nyour inventory: \n");
 			list_items(&(avatar->backpack));
+			printf("\n");
 		} else {
 			invalid_command = true;
 			printf("Not a valid command, please try again or type h for HELP: ");                                                                                                                                                                                                                                                                                                                                                             // TODO add help for command reference manual
 		}
 		// sanitizing user input
-		if (invalid_arg == -1) {
+		if (arg_num == -1) {
 			printf("\"%s\" is an invalid argument to the command \"%s\" \n", arg, command);
 		}
-	} while(invalid_command || invalid_arg == -1);
+	} while(invalid_command || arg_num == -1);
+	return arg_num;
 }
 
 /*
@@ -100,29 +100,41 @@ void get_command(Avatar *avatar) {
 
 int init_game(Avatar **player) {
 
-	Item *cell_items = useable_items("cell key", "this looks like it could be useful", "prison cell", "you open the cell door and leave",
-	                                 items("rotting skull", "poor soul; what do you think happened to him?",
-	                                       items("Jared Yeager's sandal", "", NULL)));
-	Room *prison_cell = room("prison cell", "a dark, disagreeably damp, musty, and cold room. You need to find a way out.", cell_items);
+	Item *cell_items = useable_items("cell key", "an old and rusty key; this looks like it could be useful", "prison cell", "the door of the cell creaks open, you're free!", PRISON_KEY,
+	                                 items("crushed skull", "poor soul; what do you think happened to him?",
+	                                       items("jared yeager's sandal", "if only you could get out of here, this would sell for quite the penny", NULL)));
+    Item *sewer_items = useable_items("cruddy key", "submerged in a warm, viscous liquid. Your nose flares at the toxic odor but your curiosity is peaked", "vault_chamber", "", CRUDDY_KEY,
+									  items("nic herndon's water bottle", "gives you the taste of home",
+									  		items("radioactive rat", "their squeeks make you uneasy", NULL)));
+    Item *market_items = useable_items("ornate key", "seems fitting for something important...", "vault_chamber", "It was a ruse! The key disintegrates in your hand, the vault door collapses on you and you die a tragic death", ORNATE_KEY,
+									   useable_items("apple", "crisp and refreshing, nutritious and delicious, and you can't resist to take a bite", "anywhere", "you feel refreshed", USELESS, NULL));
+
+
+	Room *prison_cell = room("prison cell", "a dark, disagreeably damp, musty, and cold prison cell. You need to find a way out.", false, cell_items);
 
 	// Item *courtyard_items =;
-	Room *courtyard = room("courtyard", "the outside of the cell, you don't know this place; better find a way out.", NULL);
+	Room *courtyard = room("courtyard", "the outside of the cell, you don't know this place; better find a way out.", true, NULL);
 
 
-	Room *guards_barracks = room("guard's quarters", "what looks like the guard's old living quarters.", NULL);
-	Room *sewer_1 = room("courtyard sewer", "a wet and dirty sewer; why would you go in here?", NULL);
-	Room *sewer_2 = room("sewer walkway", "a darker part of the sewer, out of the corner of your eye you think you spot something.", NULL);
-	Room *sewer_3 = room("market sewer", "a long and unending corridor, you hear comotion above, better investigate.", NULL);
-	Room *market = room("marketplace", "a bustling ", NULL);
-	Room *prison_walkway = room("prison walkway", "a gloomy prison walkway lit only by the torches on the walls. You see some light coming from a door up ahead", NULL);
-	Room *vault_chamber = room("vault chamber", "the chamber before the vault. The vault door is locked; I need a key", NULL);
-	Room *vault_interior = room("vaul interior", "the inside of the vault. You could have never dream of so much wealth in one place", NULL);
-	// Item *test_item = items("apple", "debugging", NULL);
-	// Item *test_use =  useable_items("key", "debug", "test", "used", NULL);
-	// main_room = connect_room(main_room, northen_room, NORTH);
-	// add_item(&(main_room->items), test_item);
-	// add_item(&(main_room->items), test_use);
-	// list_connections(main_room);
+	Room *guards_barracks = room("guard's quarters", "what looks like the guard's old living quarters.", false, NULL);
+	Room *sewer_1 = room("courtyard sewer", "a wet and dirty sewer; why would you go in here?", false, NULL);
+	Room *sewer_2 = room("sewer walkway", "a darker part of the sewer, out of the corner of your eye you think you spot something.", false, NULL);
+	Room *sewer_3 = room("market sewer", "a long and unending corridor, you hear comotion above, better investigate.", false, NULL);
+	Room *market = room("marketplace", "a bustling marketplace filled with traders from all over the world. You're relieved that nobody saw your escape.", false, NULL);
+	Room *bank = room("Herndon Bank", "a quiet and unasuming bank. The townspeople say that this is the most secure place to hold your money.", false, NULL);
+	Room *vault_chamber = room("vault chamber", "the chamber before the vault. The vault door is locked; I need a key", false, NULL);
+	Room *vault_interior = room("vaul interior", "the inside of the vault. You could have never dream of so much wealth in one place.", true, NULL);
+
+	// prison_cell = connect_room(prison_cell, courtyard, NORTH); TODO set up key
+	prison_cell = connect_room(prison_cell, courtyard, NORTH);
+	courtyard = connect_room(courtyard, guards_barracks, EAST);
+	courtyard = connect_room(courtyard, sewer_1, DOWN);
+	sewer_1 = connect_room(sewer_1, sewer_2, WEST);
+	sewer_2 = connect_room(sewer_2, sewer_3, WEST);
+	sewer_3 = connect_room(sewer_3, market, UP);
+	market = connect_room(market, bank, NORTH);
+	bank = connect_room(bank, vault_chamber, EAST);
+	vault_chamber = connect_room(vault_chamber, vault_interior, SOUTH);
 	*player = avatar(prison_cell, NULL);
 	return 0;
 }
@@ -141,39 +153,23 @@ int play_game() {
 	Avatar *player;
 	init_game(&player);
 	bool game_over;
-	// printf
-	// while (!game_over) {
-	//  get_command(player);
-	// }
-	// initialize the game environment
-	// TODO: make the rooms
-	Room *main_room = get_location(player);
-	printf("room inventory: \n");
-	list_items(&(main_room->items));
-	printf("player inventory: \n");
-	list_items(&(player->backpack));
-
-	get_command(player);
-
-	printf("room inventory: \n");
-	list_items(&(main_room->items));
-	printf("player inventory: \n");
-	list_items(&(player->backpack));
-
-	get_command(player);
-
-	printf("room inventory: \n");
-	list_items(&(main_room->items));
-	printf("player inventory: \n");
-	list_items(&(player->backpack));
-
-	get_command(player);
-
-	printf("room inventory: \n");
-	list_items(&(main_room->items));
-	printf("player inventory: \n");
-	list_items(&(player->backpack));
-	printf("\n");
+	int arg_num;
+	look(player);
+	while (!game_over) {
+	 	arg_num = get_command(player);
+		if (arg_num == PRISON_KEY) {
+			get_location(player)->connections[NORTH]->locked = false;
+			printf("\nyou can go: \n");
+			list_connections(get_location(player));
+		}
+		if (arg_num == ORNATE_KEY) {
+			game_over = true;
+		}
+		if (arg_num == CRUDDY_KEY) {
+			get_location(player)->connections[SOUTH]->locked = false;
+		}
+	}
+	free_avatar(&player);
 	return 0;
 }
 
