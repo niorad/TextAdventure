b9fa2bc49327bc63dc08a3d113e6789414182ed7
vsoltan <vsoltan@umass.edu>
2019-03-01 21:57:42 +0000

    rooms docs done

--- rooms.c
+++ rooms.c
@@ -4,17 +4,21 @@
 /*
  * function: room
  * --------------------------
- * constructs a new Room struct given the passed specifications
- *      acts as a container within the game state
  * params:
  *      *name         :  the room's identifier in the backend
  *		type          :  (char *) C string
  *
- *      *description  :  textual (visual) representation in the frontend
+ *      *description  :  visual representation of the room in the frontend
  *		type          :  (char *) C string
+ *
+ *		locked        :  determines if the room is locked or unlocked (accessible to the player)
+ *		type          :  bool
  *
  *		*items        :  dynamically allocated list of items contained within the room
  *		type          :  (Item *) pointer to Item struct
+ *
+ * constructs a new Room struct given the passed specifications
+ *      acts as a container within the game state
  *
  * returns : pointer to the new struct
  * type	   : (Room *)
@@ -24,14 +28,18 @@ Room *room(char *name, char *description, bool locked, Item *items) {
 	Room *new_room = NULL;
 	new_room = (Room *) malloc(sizeof(Room));
 
+	// checks that dynamic allocation was completed successfully
 	if (new_room == NULL) {
 		printf("malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+
+	// field assignments
 	new_room->name = name;
 	new_room->description = description;
 	new_room->locked = locked;
 	new_room->items = items;
+
 	return new_room;
 }
 
@@ -39,18 +47,19 @@ Room *room(char *name, char *description, bool locked, Item *items) {
  * function: connect_room
  * --------------------------
  * params:
- *      *room        :  room whose d connection is to be set
+ *      *room        :  room whose d connection is to be set to other_room
  *		type         :  (Room *) pointer to Room struct
  *
- *      *other_room  :  room whose opposite (d + 3) % 6 connection is to be set
+ *      *other_room  :  room whose opposite, (d + 3) % 6, connection is to be set to room
  *		type         :  (Room *) pointer to Room struct
  *
  *		*items       :  dynamically allocated list of items contained within the room
  *		type         :  (Item *) pointer to Item struct
  *
- * relates two rooms based on a passed cardinal direction;
- *      used primarily in hard-coded (OR JSON) game setup
- * ex:  connect_room(a, c, NORTH), connect_room(b, a, EAST), connect_room(a, d, UP)
+ * relates two rooms based on a passed cardinal direction
+ *      used primarily in hard-coded game setup
+ *
+ * ex:  connect_room(a, c, SOUTH), connect_room(b, a, WEST), connect_room(a, d, UP)
  *						results in the following psuedo-3D orientation:
  *	    [d]<-[a][b]
  *           [c]
@@ -60,10 +69,16 @@ Room *room(char *name, char *description, bool locked, Item *items) {
  */
 
 Room *connect_room(Room *room, Room *other_room, enum direction d) {
-	// if direction already set, can't change
+	// can't change orientation once direction is set
 	if (room->connections[d] != NULL || other_room->connections[(d + 3) % 6] != NULL) {
 		return NULL;
 	}
+
+	/*
+	 * relates the two rooms: connect_room(a, c, SOUTH) also
+	 * 		effectively executes connect_room(c, a, NORTH)
+	 */
+
 	room->connections[d] = other_room;
 	other_room->connections[(d + 3) % 6] = room;
 
@@ -74,23 +89,29 @@ Room *connect_room(Room *room, Room *other_room, enum direction d) {
  * function: list_connections
  * --------------------------
  * params:
- *      *room        :  room to have connections listed
- *		type         :  (Room *) pointer to Room struct
+ *      *room  :  room to have connections listed
+ *		type   :  (Room *) pointer to Room struct
  *
- * prints out all non-NULL connections of a given room in the order:
- *		NORTH, SOUTH, EAST, WEST, UP, DOWN
+ * prints out all existing (non-null) connections of a given room
  *
  * returns : void
  */
 
 void list_connections(Room *room) {
 	Room **cons = room->connections;
+	// array of direction names required because enum values cannot be converted to strings
 	char *dirs[6] = {"north", "east", "up", "south", "west", "down"};
 	int counter = 0;
 
-	// using the (i + 3) % 6 expression, we can print out the directions "in order"
+	/*
+ 	 * only iterates three times because three opposite direction pairs
+	 * 		(n + 3) % 6 relation allows for the printing of connections
+	 * 		in the order: NORTH, SOUTH, EAST, WEST, UP, DOWN
+	 */
+
 	for (int i = 0; i < 3; ++i) {
 		if (cons[i] != NULL) {
+			// if the room is locked, prints out indicator for better user experience
 			printf("%s %s\n", dirs[i], (cons[i]->locked ? "(locked)" : ""));
 			++counter;
 		}
@@ -100,6 +121,7 @@ void list_connections(Room *room) {
 		}
 	}
 
+	// hints that, if no connections exist, some action is required for progression
 	if (counter == 0) {
 		printf("you're trapped, find a way out!\n");
 	}
@@ -109,10 +131,10 @@ void list_connections(Room *room) {
  * function: free_room
  * -------------------
  * params:
- *      **to_free        :  room to be freed
- *		type             :  (Room **) pointer to a pointer of a Room struct
+ *      **to_free  :  room to be freed
+ *		type       :  (Room **) double pointer to a Room struct
  *
- * frees the room and all of it's items then sets its pointer to NULL
+ * frees the room and all of its items, then sets its pointer to NULL
  *
  * returns : void
  */
@@ -127,21 +149,31 @@ void free_room(Room **to_free) {
  * function: free_rooms
  * --------------------
  * params:
- *      **to_free        :  room to be freed
- *		type             :  (Room **) pointer to a pointer of a Room struct
+ *      **to_free  :  list of rooms to be freed
+ *		type       :  (Room **) pointer to a pointer of a Room struct
  *
- *		dirfrom			 :  used to help with recursion, tells what room you called from (see below)
- *		type			 :  enum direction
+ *		dirfrom	   :  used to help with recursion, tells what room you called from (see below)
+ *		type	   :  enum direction
  *
- * does a depth first search and frees each room utilizing the dirfrom enum to tell where has already been searched.
+ * leverages depth first search to free each room; the dirfrom enum tells the program which
+ * 		rooms have already been searched.
+ *
+ * avoids freeing a room twice
+ *               [ c ]
+ * 		ex) ["a"][ b ]  suppose a is already freed
+ *			     [ d ]  free_rooms(b, WEST) would be called (since a is west of b),
+ *							which would then call free_rooms(c, SOUTH) and free_rooms(d, NORTH) etc
  *
  * returns : void
  */
 
 void free_rooms(Room **to_free, enum direction dirfrom) {
 	for (int i = 0; i < 6; ++i) {
+		// if already checked continue loop
 		if (i == dirfrom) continue;
-		if ((*to_free)->connections[i] != NULL) {
+
+		// if adjacent room exists, recursive call to free all of its connections 
+		if ((*to_free)->connections[i] != NULL)
 			free_rooms(&((*to_free)->connections[i]), (i + 3) % 6);
 		}
 	}
--- rooms.h
+++ rooms.h
@@ -1,12 +1,14 @@
 
 #include "items.h"
 #define LOCKED_ROOM -2
+
 /*
  * struct: room
  * ------------
- * models a room container in the game. Has the identifier name and a textual
- *      representation description. Also has its own inventory that the player
- *      can interact with as well as an array of adjacent rooms
+ * models a room container in the game. Has the identifier 'name'; a textual
+ *      representation 'description'; an inventory 'items' that the player
+ *      can interact with; a state of being locked 'locked'; and an array of
+ * 		adjacent rooms 'connections'
  */
 
 typedef struct room {
@@ -19,9 +21,11 @@ typedef struct room {
 
 /*
  * by having each cardinal direction be (n + 3) % 6 elements
- * away from one's opposite (ex: DOWN, UP) we can save space by using just one enum
+ * 		away from one's opposite (ex: DOWN, UP) we can save space by
+ * 		using a single enum
  */
 
+// enumerates all possible directions in which a room can have a connection
 enum direction {NORTH, EAST, UP, SOUTH, WEST, DOWN, NODIR};
 
 // constructor
@@ -33,9 +37,7 @@ Room *connect_room(Room *room, Room *other_room, enum direction d);
 // prints all available connections from a given room
 void list_connections(Room *room);
 
-// prints the room for debugging purposes
-// void print_room(Room *room);
-
+// memory management at the end of the game
 void free_room(Room **to_free);
 
 void free_rooms(Room **to_free, enum direction dirfrom);
